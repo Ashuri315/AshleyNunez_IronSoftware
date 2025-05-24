01916df56b997c4c6a6cb80a7caea5da50710015
Ashuri315 <ashley.nugu@gmail.com>
2025-05-23 21:14:59 -0600

    Add project files.

---
+++ AshleyNunez_IronSoftware/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace AshleyNunez_IronSoftware
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_1ABC = new System.Windows.Forms.Button();
+            this.btn_3DEF = new System.Windows.Forms.Button();
+            this.btn_4GHI = new System.Windows.Forms.Button();
+            this.btn_5JKL = new System.Windows.Forms.Button();
+            this.btn_6MNO = new System.Windows.Forms.Button();
+            this.btn_7PQRS = new System.Windows.Forms.Button();
+            this.btn_8TUV = new System.Windows.Forms.Button();
+            this.btn_9WXYZ = new System.Windows.Forms.Button();
+            this.txt_texto = new System.Windows.Forms.TextBox();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.btn_0Space = new System.Windows.Forms.Button();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.txt_result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_1ABC
+            // 
+            this.btn_1ABC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_1ABC.Location = new System.Drawing.Point(298, 165);
+            this.btn_1ABC.Name = "btn_1ABC";
+            this.btn_1ABC.Size = new System.Drawing.Size(101, 46);
+            this.btn_1ABC.TabIndex = 0;
+            this.btn_1ABC.Text = "2 ABC";
+            this.btn_1ABC.UseVisualStyleBackColor = true;
+            this.btn_1ABC.Click += new System.EventHandler(this.showNumber);
+            // 
+            // btn_3DEF
+            // 
+            this.btn_3DEF.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_3DEF.Location = new System.Drawing.Point(414, 165);
+            this.btn_3DEF.Name = "btn_3DEF";
+            this.btn_3DEF.Size = new System.Drawing.Size(101, 46);
+            this.btn_3DEF.TabIndex = 1;
+            this.btn_3DEF.Text = "3 DEF";
+            this.btn_3DEF.UseVisualStyleBackColor = true;
+            this.btn_3DEF.Click += new System.EventHandler(this.showNumber);
+            // 
+            // btn_4GHI
+            // 
+            this.btn_4GHI.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_4GHI.Location = new System.Drawing.Point(189, 217);
+            this.btn_4GHI.Name = "btn_4GHI";
+            this.btn_4GHI.Size = new System.Drawing.Size(101, 46);
+            this.btn_4GHI.TabIndex = 2;
+            this.btn_4GHI.Text = "4 GHI";
+            this.btn_4GHI.UseVisualStyleBackColor = true;
+            this.btn_4GHI.Click += new System.EventHandler(this.showNumber);
+            // 
+            // btn_5JKL
+            // 
+            this.btn_5JKL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_5JKL.Location = new System.Drawing.Point(298, 217);
+            this.btn_5JKL.Name = "btn_5JKL";
+            this.btn_5JKL.Size = new System.Drawing.Size(101, 46);
+            this.btn_5JKL.TabIndex = 3;
+            this.btn_5JKL.Text = "5 JKL";
+            this.btn_5JKL.UseVisualStyleBackColor = true;
+            this.btn_5JKL.Click += new System.EventHandler(this.showNumber);
+            // 
+            // btn_6MNO
+            // 
+            this.btn_6MNO.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_6MNO.Location = new System.Drawing.Point(414, 217);
+            this.btn_6MNO.Name = "btn_6MNO";
+            this.btn_6MNO.Size = new System.Drawing.Size(101, 46);
+            this.btn_6MNO.TabIndex = 4;
+            this.btn_6MNO.Text = "6 MNO";
+            this.btn_6MNO.UseVisualStyleBackColor = true;
+            this.btn_6MNO.Click += new System.EventHandler(this.showNumber);
+            // 
+            // btn_7PQRS
+            // 
+            this.btn_7PQRS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_7PQRS.Location = new System.Drawing.Point(189, 269);
+            this.btn_7PQRS.Name = "btn_7PQRS";
+            this.btn_7PQRS.Size = new System.Drawing.Size(101, 46);
+            this.btn_7PQRS.TabIndex = 5;
+            this.btn_7PQRS.Text = "7 PQRS";
+            this.btn_7PQRS.UseVisualStyleBackColor = true;
+            this.btn_7PQRS.Click += new System.EventHandler(this.showNumber);
+            // 
+            // btn_8TUV
+            // 
+            this.btn_8TUV.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_8TUV.Location = new System.Drawing.Point(298, 269);
+            this.btn_8TUV.Name = "btn_8TUV";
+            this.btn_8TUV.Size = new System.Drawing.Size(101, 46);
+            this.btn_8TUV.TabIndex = 6;
+            this.btn_8TUV.Text = "8 TUV";
+            this.btn_8TUV.UseVisualStyleBackColor = true;
+            this.btn_8TUV.Click += new System.EventHandler(this.showNumber);
+            // 
+            // btn_9WXYZ
+            // 
+            this.btn_9WXYZ.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_9WXYZ.Location = new System.Drawing.Point(414, 269);
+            this.btn_9WXYZ.Name = "btn_9WXYZ";
+            this.btn_9WXYZ.Size = new System.Drawing.Size(101, 46);
+            this.btn_9WXYZ.TabIndex = 7;
+            this.btn_9WXYZ.Text = "9 WXYZ";
+            this.btn_9WXYZ.UseVisualStyleBackColor = true;
+            this.btn_9WXYZ.Click += new System.EventHandler(this.showNumber);
+            // 
+            // txt_texto
+            // 
+            this.txt_texto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_texto.Location = new System.Drawing.Point(30, 51);
+            this.txt_texto.Multiline = true;
+            this.txt_texto.Name = "txt_texto";
+            this.txt_texto.ReadOnly = true;
+            this.txt_texto.Size = new System.Drawing.Size(326, 55);
+            this.txt_texto.TabIndex = 9;
+            this.txt_texto.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn_send
+            // 
+            this.btn_send.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_send.Location = new System.Drawing.Point(414, 321);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(101, 46);
+            this.btn_send.TabIndex = 10;
+            this.btn_send.Text = "# Send";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // btn_0Space
+            // 
+            this.btn_0Space.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_0Space.Location = new System.Drawing.Point(298, 321);
+            this.btn_0Space.Name = "btn_0Space";
+            this.btn_0Space.Size = new System.Drawing.Size(101, 46);
+            this.btn_0Space.TabIndex = 11;
+            this.btn_0Space.Text = "0 Space";
+            this.btn_0Space.UseVisualStyleBackColor = true;
+            this.btn_0Space.Click += new System.EventHandler(this.showNumber);
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Delete.Location = new System.Drawing.Point(189, 321);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(101, 46);
+            this.btn_Delete.TabIndex = 12;
+            this.btn_Delete.Text = "* Delete";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clear.Location = new System.Drawing.Point(189, 165);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(101, 46);
+            this.btn_clear.TabIndex = 13;
+            this.btn_clear.Text = "1 Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // txt_result
+            // 
+            this.txt_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_result.Location = new System.Drawing.Point(384, 51);
+            this.txt_result.Multiline = true;
+            this.txt_result.Name = "txt_result";
+            this.txt_result.ReadOnly = true;
+            this.txt_result.Size = new System.Drawing.Size(326, 55);
+            this.txt_result.TabIndex = 14;
+            this.txt_result.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 435);
+            this.Controls.Add(this.txt_result);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_0Space);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.txt_texto);
+            this.Controls.Add(this.btn_9WXYZ);
+            this.Controls.Add(this.btn_8TUV);
+            this.Controls.Add(this.btn_7PQRS);
+            this.Controls.Add(this.btn_6MNO);
+            this.Controls.Add(this.btn_5JKL);
+            this.Controls.Add(this.btn_4GHI);
+            this.Controls.Add(this.btn_3DEF);
+            this.Controls.Add(this.btn_1ABC);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_1ABC;
+        private System.Windows.Forms.Button btn_3DEF;
+        private System.Windows.Forms.Button btn_4GHI;
+        private System.Windows.Forms.Button btn_5JKL;
+        private System.Windows.Forms.Button btn_6MNO;
+        private System.Windows.Forms.Button btn_7PQRS;
+        private System.Windows.Forms.Button btn_8TUV;
+        private System.Windows.Forms.Button btn_9WXYZ;
+        private System.Windows.Forms.TextBox txt_texto;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.Button btn_0Space;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.TextBox txt_result;
+    }
+}
+
---
+++ AshleyNunez_IronSoftware/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AshleyNunez_IronSoftware
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Shows the number of the button clicked in the TextBox named txt_texto
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void showNumber(Object sender, EventArgs e)
+        {
+            var button =  (Button)sender;
+            if (button.Text.ElementAt(0) == '0' )
+            {
+                if (!txt_texto.Text.EndsWith(" ") && txt_texto.Text.Length != 0)
+                {
+                    txt_texto.Text += " ";
+                }
+                
+            } else
+            {
+                txt_texto.Text += button.Text.ElementAt(0);
+            }
+               
+        }
+
+        /// <summary>
+        /// Makes the conversion from numbers to letters, with a dictionary
+        /// </summary>
+        /// <param name="input">The string with the combinations of numbers</param>
+        /// <returns></returns>
+        public static string OldPhonePad(string input)
+        {
+            Dictionary <int, string> PhonePadMap = new Dictionary<int, string>
+            {
+                {2, "A"}, {22, "B"}, {222, "C"},
+                {3, "D"}, {33, "E"}, {333, "F"},
+                {4, "G"}, {44, "H"}, {444, "I"},
+                {5, "J"}, {55, "K"}, {555, "L"},
+                {6, "M"}, {66, "N"}, {666, "O"},
+                {7, "P"}, {77, "Q"}, {777, "R"}, {7777, "S"},
+                {8, "T"}, {88, "U"}, {888, "V"},
+                {9, "W"}, {99, "X"}, {999, "Y"}, {9999, "Z"}
+            };
+            var messageArray = input.Split(' ');
+            string result = "";
+            List<string> message = new List<string>();
+            //Gets the numbers separated by space
+            foreach (var item in messageArray)
+            {
+                StringBuilder messageNumber = new StringBuilder();
+                for (global::System.Int32 i = 0; i < item.Length; i++)
+                {
+                    if (item[i].Equals('*'))
+                    {
+                        messageNumber.Clear();
+                        continue;
+                    }
+                    if (i > 0 && item[i] != item[i - 1] && messageNumber.Length > 0)
+                    {
+                        message.Add(messageNumber.ToString());
+                        messageNumber.Clear();
+                    }
+                    messageNumber.Append(item[i]);
+
+                    if (i == item.Length - 1)
+                    {
+                        message.Add(messageNumber.ToString());
+                    }
+                }
+            }
+            //Gets the combination of each number from the dictionary
+            foreach (var item in message)
+            {
+                int number = int.Parse(item.ToString());
+                if (PhonePadMap.ContainsKey(number))
+                {
+                    result += PhonePadMap[number];
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Adds an asterisk after the last number written for it to be deleted
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            string text = txt_texto.Text;
+            if (text.Length != 0 && (!(text.EndsWith(" ") || text.EndsWith("*"))))
+            {
+                txt_texto.Text += "*";
+            }
+            
+        }
+
+        /// <summary>
+        /// Sends the input written for it to be converted in letters
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            txt_result.Text = OldPhonePad(txt_texto.Text);
+        }
+
+        /// <summary>
+        /// Clear the Text wirtten in the TextBox named txt_texto and txt_result
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            txt_texto.Text = "";
+            txt_result.Text = "";
+        }
+    }
+}
